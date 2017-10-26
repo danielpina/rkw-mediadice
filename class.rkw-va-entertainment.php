08aa52ea4761a36fed7177c6f65b07dffe9d5ee9
Daniel Pina <daniel.pina@mvfglobal.com>
2017-10-26 16:37:48 +0100

    adding class file

---
+++ class.rkw-va-entertainment.php
@@ -0,0 +1,9 @@
+<?php 
+// Prevent direct file access
+defined( 'ABSPATH' ) or exit;
+
+class Rkw_Va_Entertainment {
+	private function __constructor() {
+		$dir = dirname( __FILE__ );
+	}
+}
--- rkw-va-entertainment.php
+++ rkw-va-entertainment.php
@@ -1,3 +1,30 @@
 <?php
+/*
+   Plugin Name: RKW Virtual Assitant
+   Plugin URI: https://github.com/danielpina/rkw-va-entertainment
+   Description: Virtual assistant to help you choose entertainment options.
+   Version: 1.0
+   Author: Daniel Pina
+   Author URI: http://codingdaniel.com/
+   License: MIT
+   License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+   Text Domain: rkw-va-entertainment
+   Domain Path: /languages
+   */
 
-echo "rkw-va-entertainment";
+// Prevent direct file access
+defined( 'ABSPATH' ) or exit;
+
+define( 'RKWVA_VERSION', '1.0' );
+
+require_once( plugin_dir_path( __FILE__ ) . 'includes/class.rkw-va-entertainment.php' );   
+
+function run_assistant() {
+ 
+    $rkw_assistant = new Rkw_Va_Entertainment();
+    $rkw_assistant->run();
+ 
+}
+ 
+run_assistant();
+
